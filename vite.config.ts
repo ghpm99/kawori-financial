aa412ca7befa77cc99aeeba8a918199a7cdb1645
ghpm99 <ghpm99@gmail.com>
2025-08-07 23:49:51 -0300

    feat: create project

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [react()],
+    css: {
+        modules: {
+            localsConvention: 'camelCaseOnly',
+        },
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/styles/variables.scss";`,
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.ts',
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+        },
+    },
+});
